c566b13c693d6b7c06ce53122cc106d9caba0f38
vagrant <vagrant@homestead>
2019-07-08 12:00:30 +0000

    分类话题列表

--- app/helpers.php
+++ app/helpers.php
@@ -4,3 +4,10 @@ function route_class()
 {
     return str_replace('.', '-', Route::currentRouteName());
 }
+
+
+
+public function category_nav_active($category_id){
+
+	return active_class(if_route('topics.show') && if_route_param('category' , $category_id));
+}
