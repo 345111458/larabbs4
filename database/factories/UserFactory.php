0821c1e3db80d38cc17615aca88b9d7d4e458f63
vagrant <vagrant@homestead>
2019-07-08 11:24:27 +0000

    填充用户和话题数据

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,11 +17,16 @@ use Faker\Generator as Faker;
 */
 
 $factory->define(User::class, function (Faker $faker) {
+	$updated_at = $faker->dateTimeThisMonth();
+	$created_at = $faker->dateTimeThisMonth($updated_at);
+	
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+        'created_at'	=>	$created_at,
+        'updated_at'	=>	$updated_at,
     ];
 });
--- database/seeds/TopicsTableSeeder.php
+++ database/seeds/TopicsTableSeeder.php
@@ -2,15 +2,24 @@
 
 use Illuminate\Database\Seeder;
 use App\Models\Topic;
+use App\Models\User;
+use App\Models\Category;
+
+
 
 class TopicsTableSeeder extends Seeder
 {
     public function run()
     {
-        $topics = factory(Topic::class)->times(50)->make()->each(function ($topic, $index) {
-            if ($index == 0) {
-                // $topic->field = 'value';
-            }
+    	$faker = app(Faker\Generator::class);
+
+    	$user_id = User::all()->pluck('id')->toArray();
+    	$category_id = Category::all()->pluck('id')->toArray();
+
+        $topics = factory(Topic::class)->times(500)->make()->each(function ($topic, $index) use ($faker,$user_id,$category_id) {
+            
+            $topic->user_id = $faker->randomElement($user_id);
+            $topic->category_id = $faker->randomElement($category_id);
         });
 
         Topic::insert($topics->toArray());
