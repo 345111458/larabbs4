354e1b450d65fbb9432351118806e958fcb888b5
vagrant <vagrant@homestead>
2019-07-09 02:58:33 +0000

    翻译 URL 标示

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -55,4 +55,14 @@ class Topic extends Model
     }
 
 
+    public function link($params = []){
+
+        return route('topics.show',array_merge([$this->id,$this->slug] , $params));
+    }
+
+
+
+
+
+
 }
