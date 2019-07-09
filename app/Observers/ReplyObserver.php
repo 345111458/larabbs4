55c531c2ea79bf18685f3c95b735bfac2da7be00
vagrant <vagrant@homestead>
2019-07-09 06:50:11 +0000

    用户可以发表评论

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -18,4 +18,21 @@ class ReplyObserver
     {
         //
     }
+
+
+    public function saved(Reply $reply){
+
+    	$reply->topic->reply_count = $reply->topic->replies->count();
+    	$reply->topic->save();
+    }
+
+
+    public function saving(Reply $reply){
+
+    	$reply->content = clean($reply->content , 'user_topic_body');
+    }
+
+
+
+
 }
